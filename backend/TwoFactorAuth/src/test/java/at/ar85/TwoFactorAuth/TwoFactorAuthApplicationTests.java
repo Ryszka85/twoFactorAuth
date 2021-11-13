8ca32628be269e8e7767c4b561f3fc4ef02de65a
Adrian Ryszka <adrian.ryszka@gmx.net>
2021-11-13 18:26:12 +0100

    Project Setup

---
+++ backend/TwoFactorAuth/src/test/java/at/ar85/TwoFactorAuth/TwoFactorAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ar85.TwoFactorAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoFactorAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
